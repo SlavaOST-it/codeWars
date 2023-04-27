7d809d109ba7ed0a59179115ece4a5edd7bb59ed
SlavaOST <slavaost-it@mail.ru>
2023-04-27 10:49:06 +0300

    add task

--- codeWars_1.js
+++ codeWars_1.js
@@ -232,3 +232,15 @@ function fakeBin(x) {
 }
 
 console.log(fakeBin('45385593107843568'));
+
+
+//=====================================//
+function unique(arr) {
+    return Array.from(new Set(arr))
+}
+
+let values = ["Hare", "Krishna", "Hare", "Krishna",
+    "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+console.log( unique(values))
