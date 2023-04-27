32602e01dfe853f4b456df70d43a0d8239dcf66f
SlavaOST <slavaost-it@mail.ru>
2023-04-27 10:16:04 +0300

    add task

--- codeWars_1.js
+++ codeWars_1.js
@@ -196,3 +196,21 @@ function booleanToString(b) {
 }
 
 
+//=====================================//
+function invert(array) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > 0) {
+            newArray.push(-array[i])
+        } else {
+            newArray.push(array[i] * (-1))
+        }
+    }
+    return newArray;
+}
+
+console.log(invert([1, 2, 3, 4, 5]))
+console.log(invert([-1, -2, -3, -4, -5]))
+console.log(invert([1, -2, 3, -4, 5]))
+console.log(invert([]))
+console.log(invert([0]))
