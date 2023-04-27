96972b4af939068e6bb7e02fcb5ac2468052c0f0
SlavaOST <slavaost-it@mail.ru>
2023-04-27 10:14:54 +0300

    add task

--- codeWars_1.js
+++ codeWars_1.js
@@ -177,3 +177,14 @@ console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12,
 console.log(countPositivesSumNegatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]))
 console.log(countPositivesSumNegatives(null))
 console.log(countPositivesSumNegatives([]))
+
+
+// ==================================//
+function lovefunc(flower1, flower2) {
+    return (flower1 % 2 === 0 && flower2 % 2 === 1) || (flower1 % 2 === 1 && flower2 % 2 === 0);
+}
+
+console.log(lovefunc(1, 4))
+console.log(lovefunc(2, 2))
+console.log(lovefunc(0, 1))
+console.log(lovefunc(0, 0))
