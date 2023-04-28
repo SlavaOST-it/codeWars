e4ca14c6c3064ec63075d0ed6618f706e0a2d03d
SlavaOST <slavaost-it@mail.ru>
2023-04-28 17:54:47 +0300

    add task

--- codeWars_1.js
+++ codeWars_1.js
@@ -324,3 +324,19 @@ console.log(isDivisible(3, 3, 4))
 console.log(isDivisible(12, 3, 4));
 console.log(isDivisible(8, 3, 4))
 
+
+//=====================================//
+function basicOp(operation, value1, value2) {
+    if (operation === '+') {
+        return (value1 + value2)
+    } else if (operation === '-') {
+        return (value1 - value2)
+    } else if (operation === '/') {
+        return (value1 / value2)
+    } else if (operation === '*') {
+        return (value1 * value2)
+    }
+}
+
+console.log(basicOp("+", 4, 7))
+console.log(basicOp('-', 15, 18));
