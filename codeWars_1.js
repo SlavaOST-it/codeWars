5264555404e86df993b72b2c38f7883b28c844da
SlavaOST <slavaost-it@mail.ru>
2023-04-28 17:54:24 +0300

    add task

--- codeWars_1.js
+++ codeWars_1.js
@@ -315,4 +315,12 @@ console.log(removeUrlAnchor('www.codewars.com/katas/?page=1#about'))
 console.log(removeUrlAnchor('www.codewars.com/katas/'))
 
 
+//=====================================//
+function isDivisible(n, x, y) {
+    return n % x === 0 && n % y === 0;
+}
+
+console.log(isDivisible(3, 3, 4))
+console.log(isDivisible(12, 3, 4));
+console.log(isDivisible(8, 3, 4))
 
