787ef54de088887a632f01ff2ab1230e51f6ebaa
SlavaOST <slavaost-it@mail.ru>
2023-05-02 16:50:24 +0300

    add task

--- codeWars_1.js
+++ codeWars_1.js
@@ -376,3 +376,13 @@ function greet(name) {
 }
 
 console.log(greet("Ryan"))
+
+
+//=====================================//
+
+function past(h, m, s) {
+    return ((h * 3600000) + (m * 60000) + (s * 1000))
+}
+
+console.log(past(0, 1, 1))
+console.log(past(1, 1, 1));
