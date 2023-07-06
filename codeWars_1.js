063f933d9692ccfae55e89d043e3cc5b343206b6
SlavaOST <slavaost-it@mail.ru>
2023-07-06 17:54:05 +0300

    add task

--- codeWars_1.js
+++ codeWars_1.js
@@ -573,3 +573,13 @@ console.log(XO('xxOo'))
 console.log(XO('xxxm'))
 console.log(XO('Oo'))
 console.log(XO('ooom'))
+
+
+//==================================//
+function findShort(s) {
+    return s.split` `.sort((a, b) => a.length - b.length)[0].length
+}
+
+console.log(findShort("bitcoin take over the world maybe who knows perhaps"))
+console.log(findShort("turns out random test cases are easier than writing out basic ones"))
+console.log(findShort("Let's travel abroad shall we"))
