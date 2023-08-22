cd990ab027e03aba73d4aeacaa4166b30df830fd
SlavaOST <slavaost-it@mail.ru>
2023-08-22 21:32:40 +0300

    add task

--- codeWars_1.js
+++ codeWars_1.js
@@ -643,3 +643,13 @@ console.log(isSquare(25))
 console.log(isSquare(26))
 
 
+//==================================//
+function getSum(a, b) {
+    let min = Math.min(a, b)
+    max = Math.max(a, b)
+    return (max - min + 1) * (max + min) / 2
+}
+
+console.log(getSum(0, -1))
+console.log(getSum(0, 1))
+console.log(getSum(2, 2))
