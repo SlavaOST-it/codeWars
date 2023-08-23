fc79b0047448382cb2bf11311f750de348f8734d
SlavaOST <slavaost-it@mail.ru>
2023-08-23 20:09:35 +0300

    add task

--- codeWars_1.js
+++ codeWars_1.js
@@ -699,3 +699,20 @@ function printerError(s) {
 }
 
 console.log(printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"))
+
+
+//==================================//
+// const number = function (busStops) {
+//     let arr = busStops.map(el => el[0] - el[1])
+//     let res = 0
+//     for (let i = 0; i < arr.length; i++) {
+//         res += arr[i];
+//     }
+//
+//     return res
+// }
+//
+// console.log(number([[10, 0], [3, 5], [5, 8]])) // 5
+// console.log(number([[3, 0], [9, 1], [4, 10], [12, 2], [6, 1], [7, 10]]))  //17
+// console.log(number([[3, 0], [9, 1], [4, 8], [12, 2], [6, 1], [7, 8]]))   //21
+// console.log(number([[0, 0]]))
